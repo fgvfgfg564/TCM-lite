f485deadd6c87efbbba83b94e8340e16e0cb0d43
Xinyu Hang <xyhang@pku.edu.cn>
2024-04-25 06:30:42 +0000

    update anchor run script

--- experiments/SAv1/gigapixel_webp_evcll/run.sh
+++ experiments/SAv1/gigapixel_webp_evcll/run.sh
@@ -3,4 +3,4 @@ DATASET_DIR=./images/gigapixel/*.jpg
 
 # sudo --preserve-env=PATH,PYTHONPATH,CUDA_VISIBLE_DEVICES sh -c "which python"
 
-python -u tools/tester.py "$BASEDIR" -i "$DATASET_DIR" -a SAv1 --tools WebP EVC --tool_filter WebP EVC_LL --save_image --target_bpp 0.4 0.6 0.8 1.0 --ctu_size 512 --target_time 30 45 60 90 --loss PSNR --num_steps 1000 | tee ${BASEDIR}/main.log
+python -u tools/tester.py "$BASEDIR" -i "$DATASET_DIR" -a SAv1 --tools WebP EVC --tool_filter WebP EVC_LL --save_image --target_bpp 1.5 2.0 2.5 3.0 --ctu_size 512 --target_time 60 90 120 180 --loss PSNR --num_steps 10000 | tee ${BASEDIR}/main.log
