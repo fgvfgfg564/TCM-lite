cd3c57f02f5402c3fa1d2e11ede239f56d40af49
Xinyu Hang <xyhang@pku.edu.cn>
2024-06-12 05:29:15 +0000

    update anchor tests

--- experiments/anchors/classa/evc/run.sh
+++ experiments/anchors/classa/evc/run.sh
@@ -3,4 +3,4 @@ DATASET_DIR=~/dataset/NIC_Dataset/test/ClassA_6K/*.png
 
 export PYTHONPATH=.:..:coding_tools/MLIC/MLICPP
 
-python -u tools/tester.py "$BASEDIR" -i "$DATASET_DIR" -a EVC --save_image --quality 0.0 0.25 0.5 0.75 1.0 | tee ${BASEDIR}/main.log
+python -u tools/tester.py "$BASEDIR" -i "$DATASET_DIR" -a EVC --save_image --quality 0.0 0.05 0.1 0.15 0.2 0.25 0.5 0.75 1.0 | tee ${BASEDIR}/main.log
